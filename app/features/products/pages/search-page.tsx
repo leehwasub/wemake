af0b5f054ba8ded84c72f89f741eb9d0e078772e
hwasub <hwasub1115@naver.com>
2025-04-19 16:43:47 +0900

    3-20 Search Pages

--- app/features/products/pages/search-page.tsx
+++ app/features/products/pages/search-page.tsx
@@ -1,15 +1,55 @@
-export function loader({ request }: any) {
-  return {};
+import { z } from "zod";
+import type { Route } from "./+types/search-page";
+import { Hero } from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import { ProductPagination } from "~/common/components/product-pagination";
+import { Form } from "react-router";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
+
+export const meta: Route.MetaFunction = () => {
+  return [
+    {title: `Search Proudcts | wemake`},
+    {name: "description", content: "Search for products on wemake"},
+  ]
 }
 
-export function action({ request }: any) {
-  return {};
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
+
+export function loader({request} : Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const {success, data: parsedData} = paramsSchema.safeParse(Object.fromEntries(url.searchParams));
+  if (!success) {
+    throw new Error("InValid params");
+  }
+  console.log(parsedData);
 }
 
-export function meta({ data }: any) {
-  return { title: "Search Page" };
-}
-
-export default function SearchPage() {
-  return <div>Search Page</div>;
+export default function SearchPage({loaderData} : Route.ComponentProps) {
+  return (
+  <div className="space-y-20">
+    <Hero title="Search" subtitle="Search for products on wemake" />
+    <Form className="flex justify-center max-w-screen-sm items-center gap-2 mx-auto">
+      <Input name="query" placeholder="Search for products" className="text-lg" />
+      <Button type="submit">Search</Button>
+    </Form>
+    <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            productId={`productId-${index}`}
+            productName={`Product Name ${index}`}
+            productDescription={`Product Description ${index}`}
+            messageCount={12}
+            viewCount={12}
+            upvoteCount={120}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10}/>
+  </div>
+  );
 } 
