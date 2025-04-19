ed20bd8fee4f3ccffa40c4b931e6a2d1bbbabd23
hwasub <hwasub1115@naver.com>
2025-04-19 23:49:15 +0900

    3-25 Product Overview Page

---
+++ app/features/products/pages/product-reviews-page.tsx
@@ -0,0 +1,15 @@
+export function loader({ request }: any) {
+  return {};
+}
+
+export function action({ request }: any) {
+  return {};
+}
+
+export function meta({ data }: any) {
+  return { title: "Product Reviews Page" };
+}
+
+export default function ProductReviewsPage() {
+  return <div>Product Reviews Page</div>;
+} 
