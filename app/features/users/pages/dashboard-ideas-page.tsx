07195cdc84ace4108532cd4911b82f651754adad
hwasub <hwasub1115@naver.com>
2025-04-28 00:07:36 +0900

    3-45 Dashboard Page

--- app/features/users/pages/dashboard-ideas-page.tsx
+++ app/features/users/pages/dashboard-ideas-page.tsx
@@ -1,3 +1,4 @@
+import { IdeaCard } from "~/features/ideas/components/idea-card";
 import type { Route } from "./+types/dashboard-ideas-page";
 
 export const meta: Route.MetaFunction = () => {
@@ -7,5 +8,23 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function DashboardIdeasPage() {
-  return <div>Dashboard Ideas Page</div>;
+  return (
+    <div className="space-y-5 h-full">
+      <h1 className="text-2xl font-semibold mb-6">Claimed Ideas</h1>
+      <div className="grid grid-cols-4 gap-6">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <IdeaCard
+            ideaId="ideaId"
+            title="A startup that creates an AI-powered generated personl trainer, 
+            delivering customized fitness recommendations and tracking of progress using a mobile app to track workout
+            s and progress as well as a website to manage the business."
+            viewCount={123}
+            timeAgo="12 hours ago"
+            likeCount={123}
+            claimed={false}
+            />
+        ))}
+      </div>
+    </div>
+  );
 } 
