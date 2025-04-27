72a1be85a47e243eaf65a4d58bc1fe9f06a7e824
hwasub <hwasub1115@naver.com>
2025-04-28 00:46:10 +0900

    3-46 Dashboard Analytics

--- app/features/users/pages/dashboard-page.tsx
+++ app/features/users/pages/dashboard-page.tsx
@@ -1,4 +1,8 @@
+import { IdeaCard } from "~/features/ideas/components/idea-card";
 import type { Route } from "./+types/dashboard-page";
+import { Card, CardContent, CardHeader, CardTitle } from "~/common/components/ui/card";
+import { ChartContainer, ChartTooltip, ChartTooltipContent, type ChartConfig } from "~/common/components/ui/chart";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -6,6 +10,63 @@ export const meta: Route.MetaFunction = () => {
   ];
 };
 
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+]
+const chartConfig = {
+  views: {
+    label: "Views",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold mb-6">Dashboard</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Profile views</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey="views"
+                type="natural"
+                stroke="var(--color-views)"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      
+    </div>
+  );
 } 
