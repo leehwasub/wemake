1f6f651ba0f095ac8a2a3410633718270429deea
hwasub <hwasub1115@naver.com>
2025-05-05 00:52:54 +0900

    7-8 Social Login Triggers

--- app/features/auth/pages/social-complete-page.tsx
+++ app/features/auth/pages/social-complete-page.tsx
@@ -1,3 +1,31 @@
+import { z } from "zod";
+import type { Route } from "./+types/social-complete-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
+
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({request, params}: Route.LoaderArgs) => {
+  const {success, data} = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const { provider } = data;
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return redirect("/auth/login");
+  }
+  const { client, headers } = makeSSRClient(request);
+  const {error} = await client.auth.exchangeCodeForSession(code);
+  if (error) {
+    throw error;
+  }
+  return redirect("/", {headers});
+}
+
 export default function SocialCompletePage() {
   return <div>Social Complete Page</div>;
 } 
--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,3 +1,35 @@
+import { z } from "zod";
+import type { Route } from "./+types/social-start-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
+
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({request, params}: Route.LoaderArgs) => {
+  const {success, data} = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const redirectTo = `http://localhost:5173/auth/social/${data.provider}/complete`;
+  const { provider } = data;
+  const { client, headers } = makeSSRClient(request);
+  const {data: {url}, error} = await client.auth.signInWithOAuth({
+    provider,
+    options:{
+      redirectTo,
+    }
+  });
+  if (url) {
+    return redirect(url, {headers});
+  }
+  if (error) {
+    throw error;
+  }
+  return redirect("/auth/login", {headers});
+}
+
 export default function SocialStartPage() {
   return <div>Social Start Page</div>;
 } 
