c8928bfb9b4159457f9be43e980982b1801e26fa
hwasub <hwasub1115@naver.com>
2025-05-09 01:34:48 +0900

    9-4 Notification Triggers

--- app/features/users/schema.ts
+++ app/features/users/schema.ts
@@ -26,8 +26,8 @@ export const profiles = pgTable("profiles", {
 });
 
 export const follows = pgTable("follows", {
-  follower_id: uuid().references(() => profiles.profile_id, {onDelete: "cascade"}),
-  following_id: uuid().references(() => profiles.profile_id, {onDelete: "cascade"}),
+  follower_id: uuid().references(() => profiles.profile_id, {onDelete: "cascade"}).notNull(),
+  following_id: uuid().references(() => profiles.profile_id, {onDelete: "cascade"}).notNull(),
   created_at: timestamp().notNull().defaultNow(),
 });
 
